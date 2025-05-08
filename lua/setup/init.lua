93123928c868baeadcfd715f7c5e7816c504df58
prime-run <prime0run@gmail.com>
2025-05-08 06:48:38 +0000

    fix ya maps

--- lua/setup/init.lua
+++ lua/setup/init.lua
@@ -79,7 +79,9 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
-vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set('v', '<leader>p', [["_dP]])
+-- C and rust - middle finger to auto tag XD
+vim.keymap.set({ 'i', 'v' }, '<C-;>', '<Esc>$a;')
 
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
